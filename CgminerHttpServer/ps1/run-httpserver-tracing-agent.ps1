2fbef071c83f74a2d7e0b1b0083ec745fa5a8607
jtconnors <git@jtconnors.com>
2020-12-09 00:38:46 -0500

    Added ps1\run-httpserver-tracing-agent.ps1 for what it's worth

---
+++ CgminerHttpServer/ps1/run-httpserver-tracing-agent.ps1
@@ -0,0 +1,47 @@
+
+####################
+#
+# All Scripts should have this preamble     
+#
+Set-variable -Name CMDLINE_ARGS -Value $args
+
+#
+# Move to the directory containing this script so we can source the env.ps1
+# properties that follow
+#
+$STARTDIR = pwd | Select-Object | %{$_.ProviderPath}
+cd $PSScriptRoot
+
+#
+# Common properties shared by scripts
+#
+. .\env.ps1
+if ($Global:JUST_EXIT -eq "true") {
+    cd $STARTDIR
+    Exit 1
+}
+#
+# End preamble
+#
+####################
+
+#
+# Run the Java command
+#
+Set-Variable -Name TRACING_AGENT -Value "-agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image/"
+$TRACING_AGENT += $MAINCLASS
+Set-Variable -Name JAVA_ARGS -Value @(
+    """$TRACING_AGENT""",
+    '-classpath',
+    """$CLASSPATH""",
+    """$MAINCLASS""",
+    '-cgminerHost:jtconnors.com',
+    '-cgminerPort:4028',
+    '-httpPort:8000'
+)
+Exec-Cmd("$env:JAVA_HOME\bin\java.exe", $JAVA_ARGS)
+
+#
+# Return to the original directory
+#
+cd $STARTDIR
